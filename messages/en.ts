4cc663223abe69e931c4fc02c6a3814dfeeab103
Aaron Velasco Lopez <tiquismiquisss@gmail.com>
2025-02-25 22:33:22 +0100

    feat: locales

--- messages/en.ts
+++ messages/en.ts
@@ -1,7 +1,7 @@
 import authEn from "./auth/en.json";
+import errorEn from "./error/en.json";
 import languagesEn from "./languages/en.json";
 import sharedEn from "./shared/en.json";
-import errorEn from "./error/en.json";
 
 const messages = {
     ...authEn,
--- messages/es.ts
+++ messages/es.ts
@@ -1,7 +1,7 @@
 import authEs from "./auth/es.json";
+import errorEs from "./error/es.json";
 import languagesEs from "./languages/es.json";
 import sharedEs from "./shared/es.json";
-import errorEs from "./error/es.json";
 
 const messages = {
     ...authEs,
